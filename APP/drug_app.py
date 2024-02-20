a942e234f3a52cab80d831023990f9c16d781621
Venura Pussella <137409412+Venura-94@users.noreply.github.com>
2024-02-20 21:20:14 +0530

    Article changed

--- APP/drug_app.py
+++ APP/drug_app.py
@@ -42,7 +42,7 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
+article = "Thanks to the documentation guide **[of Abid Ali Awan](https://www.datacamp.com/portfolio/kingabzpro)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
 
 
 gr.Interface(
